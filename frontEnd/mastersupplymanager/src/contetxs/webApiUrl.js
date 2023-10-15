a1b9ea8831571d022a541a3e557aa4eadbcd6a34
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-10-15 20:40:45 -0300

    Delete de item lista da os feito

--- frontEnd/mastersupplymanager/src/contetxs/webApiUrl.js
+++ frontEnd/mastersupplymanager/src/contetxs/webApiUrl.js
@@ -1,5 +1,5 @@
 
 //NOTEBOOK WORK
-// export const url = "https://192.168.2.104:7068/api/v1"
+export const url = "https://192.168.2.104:7068/api/v1"
 //PC CASA
-export const url = "https://192.168.100.216:7068/api/v1"
+// export const url = "https://192.168.100.216:7068/api/v1"
