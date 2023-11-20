ad46e93be19095efc0ee42b8870686c723a2073b
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-11-19 22:15:24 -0300

    Concluida covnersão front para nextjs

---
+++ frontendnextjs/src/app/componentes/Header.tsx
@@ -0,0 +1,23 @@
+"use client"
+import React from "react";
+import Image from "next/image";
+
+
+
+const Header= ()=>{
+
+return(
+
+    <header  className=" h-24 bg-master_black">
+
+<div className="ml-1">
+    <Image  className="py-5" src={require('../assets/logo.png')} width={132} height={113} alt="logo master" />
+</div>
+
+    </header>
+
+)
+
+
+}
+export default Header;
---
+++ frontendnextjs/src/app/update-material/[materialId]/page.tsx
@@ -0,0 +1,285 @@
+"use client"
+
+import { Button } from "@nextui-org/react";
+
+import Header from "../../componentes/Header";
+import { Snackbar } from '@mui/material';
+
+import { useEffect, useState } from "react";
+import { DatePicker } from "@mui/x-date-pickers";
+import "dayjs/locale/pt-br";
+import { url } from "../../api/webApiUrl";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import MuiAlert from "@mui/material/Alert";
+import { useRouter } from "next/router";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+import dayjs from "dayjs";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
+export default function UpdateMaterial({params}:any){
+    //Variável que é passada pela rota na tela de criar material,aonde quando clicar no icone de editar,passara o id do material
+    // const route = useRouter()
+
+  const[categoria,setCategoria]=useState("")
+  const [descricao,setDescricao] = useState("")
+  const [codigoInterno,setCodigoInterno] = useState("")
+  const [codigoFabricante,setCodigoFabricante] = useState("")
+  const [marca,setMarca] = useState("")
+  const [ tensao,setTensao] = useState("")
+  const [corrente,setCorrente] = useState("")
+  const [localizacao,setLocalizacao] = useState("")
+  const [ unidade,setUnidade] = useState("")
+  const[dataentrada,setDataentrada] = useState(undefined)
+  const [openSnackBar,setOpenSnackBar]= useState(false)
+  const [ messageAlert,setMessageAlert] = useState();
+  const [ severidadeAlert,setSeveridadeAlert] = useState()
+   const [idCategoria,setIdCategoria] = useState()
+  const[oldCategory,setOldCategory]= useState()
+  const [materiais, setMateriais] = useState([]);
+  const[precoCusto,setPrecoCusto] = useState()
+  const[markup,setMarkup] = useState()
+ 
+  const unidadeMaterial = ["UN","RL","PC","MT","P"]
+  const tensoes = ["","12V","24V","127V","220V","380V","440V","660V"]
+ 
+ 
+ 
+ useEffect(()=>{
+ 
+    console.log(params.materialId)
+    getMaterial(params.materialId).then().catch()
+     
+ 
+ 
+ },[])
+ //esta função serve para verificar se o item é nulo,aonde quando importamos os dados do excel os dados vem como nulo
+ //e para realizar a  edição aqui
+ const verifyNull = (item)=>{
+ 
+   return item==null?"":item
+ 
+ }
+ 
+ 
+  const getMaterial = async(id)=>{
+ 
+  axios.get(`${url}/Materiais/${id}`).then(r=>{
+ console.log(r.data.dataEntradaNF)
+  setDataentrada(r.data.dataEntradaNF==undefined?undefined:dayjs(r.data.dataEntradaNF))
+ setCodigoInterno(r.data.id)
+ setUnidade(verifyNull(r.data.unidade))
+ setCodigoFabricante(verifyNull(r.data.codigoFabricante))
+ setCorrente(verifyNull(r.data.corrente))
+ setMarca(verifyNull(r.data.marca))
+ setDescricao(verifyNull(r.data.descricao))
+ setOldCategory(verifyNull(r.data.categoria))
+ setLocalizacao(verifyNull(r.data.localizacao))
+ setPrecoCusto(verifyNull(r.data.precoCusto))
+ setMarkup(verifyNull(r.data.markup))
+ 
+ setTensao(verifyNull(tensoes[tensoes.findIndex((x)=>x==r.data.tensao)]))
+ 
+  })
+ 
+  }
+ 
+ 
+ const handleUpdateMaterial=  async (id)=>{
+ console.log(dataentrada)
+ console.log(precoCusto)
+ console.log(markup)
+ if(precoCusto == undefined && markup==undefined){
+   console.log("FOI")
+   setPrecoCusto(0)
+   setMarkup(0)
+ }
+   // o regex esta para remover os espaços extras entre palavras,deixando somente um espaço entre palavras
+ const material = {
+     id:id,
+     codigoInterno:"",
+     codigoFabricante:codigoFabricante.trim().replace(/\s\s+/g, ' '),
+     categoria: oldCategory.trim().replace(/\s\s+/g, " "),
+     descricao:descricao.trim().replace(/\s\s+/g, ' '),
+     marca:marca.trim().replace(/\s\s+/g, ' '),
+     corrente:corrente.trim().replace(/\s\s+/g, ' '),
+     unidade:unidade,
+     tensao:tensao,
+     localizacao:localizacao.trim().replace(/\s\s+/g, ' '),
+     dataEntradaNF:dataentrada,
+     precoCusto:precoCusto==""?null:precoCusto,
+     markup:markup==""?null:markup,
+     }
+ 
+ 
+   const materialAtualizado =  await axios.put(`${url}/Materiais/${id}`,material)
+   .then(r=>
+     {  
+       console.log(r)
+       setOpenSnackBar(true)
+       setSeveridadeAlert("success")
+       setMessageAlert("Material Atualizado com sucesso")
+    
+ 
+ }
+     
+   ).catch(e=>{
+     console.log(e)
+  
+     
+     if (e.response.data.message == "Código interno já existe") {
+       setOpenSnackBar(true);
+       setSeveridadeAlert("error");
+       setMessageAlert("Já existe um material com este mesmo código interno");
+     } else if (
+       e.response.data.message ==
+       "Código de fabricante já existe"
+     ) {
+       setOpenSnackBar(true);
+       setSeveridadeAlert("error");
+       setMessageAlert("Já existe um material com este mesmo código de fabricante");
+     }
+ 
+   })
+  
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+   return (
+     <>
+ 
+
+ 
+     <h1  className='text-center font-bold text-2xl mt-6'>Editando {descricao}  (Codigo Interno: {codigoInterno}) </h1>
+   
+     <div className=' w-full flex flex-row justify-center mt-6 ' >
+ 
+ 
+     <TextField  value={oldCategory} style={{marginTop:'40px',marginLeft:'20px',marginRight:'20px'}}  
+   onChange={e=>setOldCategory(e.target.value)} label='Categoria' required/>
+     
+ 
+ 
+     {/* <TextField disabled={true}   value={codigoInterno} style={{marginTop:'40px',marginLeft:'20px',marginRight:'20px'}}
+     className={updateMaterial.inputs} onChange={e=>setCodigoInterno(e.target.value)} label='Cod Interno'  /> */}
+ 
+     <TextField    value={codigoFabricante} style={{marginTop:'40px',marginLeft:'20px',marginRight:'20px'}}
+      onChange={e=>setCodigoFabricante(e.target.value)} label='Cod Fabricante'  />
+ 
+    
+     {/* <InputText className='inputs' value={descricao} onChange={e=>setDescricao(e.target.value)} /> */}
+     <TextField   value={descricao} style={{marginTop:'40px',marginLeft:'20px',marginRight:'20px'}}
+        onChange={(e) => setDescricao(e.target.value)} label='Descrição' required />
+ 
+    
+     <TextField   value={marca} style={{marginTop:'40px',marginLeft:'20px',marginRight:'20px'}} 
+      onChange={e=>setMarca(e.target.value)}  label='Marca' />
+ 
+       <TextField
+           value={localizacao}
+           style={{ marginTop: "40px", marginLeft: "20px", marginRight: "20px" }}
+           
+           onChange={(e) => setLocalizacao(e.target.value)}
+           label="Localização"
+         />
+   </div>
+
+      <div className=' w-full flex flex-row justify-center mt-6 ' >
+           <TextField
+         value={precoCusto}
+         style={{ marginTop: "40px", marginLeft: "20px", marginRight: "20px" }}
+        
+         onChange={(e) => setPrecoCusto(e.target.value)}
+         label="Preço Custo"
+       />
+       <TextField
+           value={markup}
+           style={{ marginTop: "40px", marginLeft: "20px", marginRight: "20px" }}
+      
+           onChange={(e) => setMarkup(e.target.value)}
+           label="Markup %"
+         />
+    <Select
+      style={{ marginTop: "40px", marginLeft: "20px", marginRight: "20px" ,width:"100px"}}
+      labelId="demo-simple-select-label"
+     value={tensao}
+     label="Tensao"
+     onChange={x=>setTensao(x.target.value)}
+   >
+       {tensoes.map((x)=>(
+         <MenuItem value={x}>{x}</MenuItem>
+         
+       ))}
+     
+   
+   </Select>
+         <TextField
+           value={corrente}
+           style={{ marginTop: "40px", marginLeft: "20px", marginRight: "20px" ,width:"100px"}}
+         
+           onChange={(e) => setCorrente(e.target.value)}
+           label="Corrente"
+         />
+ 
+      
+ 
+   <Select
+      style={{ marginTop: "40px", marginLeft: "20px", marginRight: "20px" ,width:"120px"}}
+      labelId="demo-simple-select-label"
+     value={unidade}
+     label="Unidade"
+     onChange={x=>setUnidade(x.target.value)}
+   >
+       {unidadeMaterial.map((x)=>(
+         <MenuItem value={x}>{x}</MenuItem>
+         
+       ))}
+     
+   
+   </Select>
+     <div style={{marginTop:'40px',width:'206px'}}>
+ 
+     <LocalizationProvider   dateAdapter={AdapterDayjs} adapterLocale="pt-br" >
+     
+         <DatePicker  label="Data Entrada NF"  value={dataentrada} onChange={e=>setDataentrada(e)} />
+     
+     </LocalizationProvider>
+     </div>
+     
+ 
+   
+     </div>
+
+     <div className='text-center mt-8'>
+     <Button  onPress={x=>handleUpdateMaterial(params.materialId)} className='bg-master_black text-white p-4 rounded-lg font-bold text-2xl '>
+       Atualizar Material
+      </Button>
+      </div>
+    
+     <Snackbar open={openSnackBar} autoHideDuration={3000} onClose={e=>setOpenSnackBar(false)}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center'
+      }}
+     >
+             <MuiAlert onClose={e=>setOpenSnackBar(false)} severity={severidadeAlert} sx={{ width: '100%' }}>
+              {messageAlert}
+            </MuiAlert>
+            </Snackbar>
+   
+ 
+     
+     </>
+     
+     
+   );
+}
