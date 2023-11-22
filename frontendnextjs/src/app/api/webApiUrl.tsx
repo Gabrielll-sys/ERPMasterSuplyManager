dd668b488d9af6cdb161c9b81e3284556d1f4941
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-11-22 16:23:33 -0300

    Trocando url da api

--- frontendnextjs/src/app/api/webApiUrl.tsx
+++ frontendnextjs/src/app/api/webApiUrl.tsx
@@ -1,7 +1,7 @@
 
 //NOTEBOOK WORK
-export const url = "https://192.168.2.102:7068/api/v1"
+// export const url = "https://192.168.2.102:7068/api/v1"
 //PC CASA
 // export const url = "https://192.168.100.216:7068/api/v1"
 //URL .env
-// export const url = process.env.NEXT_PUBLIC_URL_API
+export const url = process.env.NEXT_PUBLIC_URL_API
