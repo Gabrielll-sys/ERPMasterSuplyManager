bb5c7767de9de6a3e241c15650892d7eae4d41e6
Gabriel Lopes <gabrielpuneco@gmail.com>
2024-01-18 14:54:25 -0300

    Implementando MOdels de notas fiscal e fornenccedor

---
+++ SupplyManager/Models/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿namespace SupplyManager.Models
+{
+    public class Fornecedor
+    {
+
+        public string? Nome { get; set; }
+        public string? Endereco { get; set; }
+        public string? Numero { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cep { get; set; }
+        public string? Cidade { get; set; }
+        public string? Estado { get; set; }
+        public string? Telefone { get; set; }
+
+
+
+
+    }
+}
