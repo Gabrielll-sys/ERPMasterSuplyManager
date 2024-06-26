fd846a0c95bc8f89245b6d3b418961a042d0885d
Gabriel Lopes <gabrielpuneco@gmail.com>
2024-06-25 21:46:11 -0300

    Trocando contato que era empresa,propriedade,para de fato empresa no RelatórioDiário

--- BackEnd/MasterErp.Services/obj/Debug/net8.0/MasterErp.Services.AssemblyInfo.cs
+++ BackEnd/MasterErp.Services/obj/Debug/net8.0/MasterErp.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterErp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e3f13094bd6e0a09aeb9552b35972f16472f7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df576029e01b29d96838b4e4f8961122d7fa1618")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterErp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterErp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
