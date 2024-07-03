0269af49499fdf8c0944cd816da50bce4896649b
Gabriel Lopes <53120770+Gabrielll-sys@users.noreply.github.com>
2024-07-03 13:20:51 -0300

    Update page.tsx

--- frontend/src/app/(update)/update-material/[materialId]/page.tsx
+++ frontend/src/app/(update)/update-material/[materialId]/page.tsx
@@ -174,7 +174,7 @@ const handleImageUploadResponse = async (urlImagem: string) => {
  //Função para calcular o markup ja trocando as virgulas por pontos,pois a variável é string,para permitir usar , ao invés de .
  const  calcularMarkup= ()=>
  {
-  console.log(Number(precoVenda))
+  
   const PRECO_CUSTO = Number(precoCusto)
 
   const PRECO_VENDA = Number(precoVenda)
